bc1c7e645b2a8b8d58b1ea2c2c797d5b52c5c2cd
Joshua Peisach <itzswirlz2020@outlook.com>
2024-06-06 14:41:42 -0400

    register secure connection and matchmakingext protcol

--- nex/register_common_secure_server_protocols.go
+++ nex/register_common_secure_server_protocols.go
@@ -1,5 +1,12 @@
 package nex
 
+import (
+	"github.com/ItzSwirlz/pokemon-gen7/globals"
+	matchmakingext "github.com/PretendoNetwork/nex-protocols-common-go/matchmaking-ext"
+	secureconnection "github.com/PretendoNetwork/nex-protocols-common-go/secure-connection"
+)
+
 func registerCommonSecureServerProtocols() {
-	// TODO: this once necessary
+	secureconnection.NewCommonSecureConnectionProtocol(globals.SecureServer)
+	matchmakingext.NewCommonMatchMakingExtProtocol(globals.SecureServer)
 }
